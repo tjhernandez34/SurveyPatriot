19524361ff84ceb50e9aeac66746b52a67c80547
Travis Hernandez & Zach Pflederer <pair+tjhernandez34.zpfled@devbootcamp.com>
2014-07-04 13:01:29 -0500

    add test

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -1,3 +1,7 @@
+before '/surveys' do
+  # cehck if logged in, or else redirect home
+end
+
 get '/surveys' do
   @surveys = Survey.all
 
@@ -5,10 +9,31 @@ get '/surveys' do
 end
 
 post '/surveys/create' do
-  # survey = Survey.create
-  # # question1=Question.create
+  @survey = Survey.create(params[:survey])
+
+  question1=Question.create(params[:question1])
+  question2=Question.create(params[:question2])
+  question3=Question.create(params[:question3])
+
+  [question1, question2, question3].each do |q|
+    (1..3).each do |num|
+      q.choices << Choice.create({
+        question_id: q.id,
+        choice: params["choice#{num}".to_sym]
+        })
+      @survey.questions << q
+    end
+  end
+
+redirect to "/surveys/#{@survey.id}"
+end
+
+get '/surveys/:survey_id' do
+@survey = Survey.find(:survey_id)
 
 
+
+end
 
 
 

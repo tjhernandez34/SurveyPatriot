11e2e3ba9474ac667d1a374588fbd046a8c32a0a
Stuart Pearlman <stupearlman@Gmail.com>
2014-07-04 13:02:00 -0500

    list all surveys

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -1,26 +1,24 @@
 get '/surveys' do
   @surveys = Survey.all
-
   erb :surveys
 end
 
 post '/surveys/create' do
   # survey = Survey.create
   # # question1=Question.create
-
-
-
-
-
-
-
-
-
-
-
-
-
 end
+
+
+
+
+
+
+
+
+
+
+
+
 
 # params
 # { survey: {

b017153bb293b8049e86f327f9b0b265a307dd41
Stuart Pearlman <stupearlman@Gmail.com>
2014-07-04 17:57:54 -0500

    MvP

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -15,13 +15,17 @@ end
 get '/surveys/:survey_id' do
   @survey = Survey.find(params[:survey_id])
   session[:round_id] = Round.create(user_id: session[:id], survey_id: params[:survey_id]).id
+  @survey_length = @survey.questions.length
+  @counter = 1
   erb :single_survey
 end
 
 post '/surveys/:survey_id' do
   @round = Round.find(session[:round_id])
-  @choice = Choice.find(params[:answer])
-  @round.choices << @choice
+  params[:answer].each_value do |choice_id|
+    @choice = Choice.find(choice_id)
+    @round.choices << @choice
+  end
   @round.save!
-  halt 200
+  redirect to "/users/#{session[:id]}"
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
+var counter = 1
 
+  $("#start_survey").on('click', function(event) {
+    event.preventDefault;
+    $("#start_survey").hide();
+    $('#question_1').show();
+  });
 
+  $("#next_question").on('click', function(event) {
+    event.preventDefault;
+    $('#question_' + counter).hide();
+    counter += 1;
+    $('#question_' + counter).show();
+  });
 
 });

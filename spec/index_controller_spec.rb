9115e20d6a87a96cc623312e8f042724245553ef
Travis Hernandez & Zach Pflederer <pair+tjhernandez34.zpfled@devbootcamp.com>
2014-07-04 14:10:10 -0500

    test index route

--- spec/index_controller_spec.rb
+++ spec/index_controller_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 describe 'IndexController' do
   describe "get '/'" do
-
+    it 'renders the index template' do
+      get '/'
+      expect(parsed_body.css('.login-form')).to_not be_empty
+    end
   end
 end

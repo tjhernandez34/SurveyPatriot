525104f5bdec2b716b16b1dfa6085621c1836ad0
Travis Hernandez <tjhernandez34@gmail.com>
2014-07-04 13:46:36 -0500

    created survey route

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -24,12 +24,12 @@ post '/surveys/create' do
     end
   end
 
-redirect to "/surveys/#{@survey.id}"
+  redirect to "/surveys/#{@survey.id}"
 end
 
 get '/surveys/:survey_id' do
-@survey = Survey.find(:survey_id)
+  @survey = Survey.find(params[:survey_id])
 
 
-
+  erb :survey
 end

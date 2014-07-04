4ee427f77b58dc22953afac100ed4a0309d999b4
Travis Hernandez & Zach Pflederer <pair+tjhernandez34.zpfled@devbootcamp.com>
2014-07-04 13:35:53 -0500

    test that each survey question is shown on survey.erb

--- spec/survey_controller_spec.rb
+++ spec/survey_controller_spec.rb
@@ -5,6 +5,8 @@ describe 'SurveyController' do
   describe "get '/surveys'" do
     let(:user) { User.create(name: 'Ron', email: 'ron@swanson.com', password: "foo") }
     let(:survey) { Survey.create(title: 'Chifago', user_id: user.id) }
+    let(:question1) { Question.create(question: 'WTF?') }
+    let(:question2) { Question.create(question: 'LOL') }
 
     it 'returns 200 status code' do
       get '/surveys'
@@ -15,6 +17,13 @@ describe 'SurveyController' do
       get '/'
       p survey.title
       expect(parsed_body.css('.survey-title')[0]).to eq("Chifago")
+    end
+
+    it 'displays all questions for each survey' do
+      [question1, question2].each { |q| survey.questions << q }
+      get '/'
+      p question1.question
+      expect(parsed_body.css('.questions li')[0]).to eq("WTF?")
     end
   end
 end

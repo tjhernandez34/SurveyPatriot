5d0a308906128e024079279eb80fb8283f876b99
Zach Pflederer <zpfled@gmail.com>
2014-07-06 14:17:03 -0500

    autofocus on each new question

--- public/js/createSurvey.js
+++ public/js/createSurvey.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
 
-	var questionHTML, choiceHTML;
+	var questionHTML, choiceHTML, choiceCount;
 	var questionCount = 0;
-	var choiceCount = 0;
 
 	$('.create-survey-section').on('click', '#add-question', function(){
 		questionCount++;
-		questionHTML = '<article class="question-container"><ul class="question-end"><li><input class="form-field" name="questions[question_' + questionCount + '][question]" type="text" placeholder="ask a question" autofocus></li><li><span class="btn btn-primary add-choice">add a choice</span><span class="btn btn-secondary rm-question">remove this question</span></li></ul></article>';
-		$('.survey-title').after(questionHTML);
+		choiceCount = 0;
+		questionHTML = '<article class="question-container"><ul class="question-end"><li><input id="question' + questionCount + '" class="form-field" name="questions[question_' + questionCount + '][question]" type="text" placeholder="ask a question"></li><li><span class="btn btn-primary add-choice">add a choice</span><span class="btn btn-secondary rm-question">remove this question</span></li></ul></article>';
+		$('.survey-end').before(questionHTML);
+		$('#question' + questionCount).focus();
 	});
 
 	$('.create-survey-section').on('click', '.add-choice', function(){

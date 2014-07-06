022498a04bd9ef7169d24823f45dcf13d750b3f6
Stuart Pearlman <stupearlman@Gmail.com>
2014-07-05 20:02:02 -0500

    Merge

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -17,7 +17,7 @@ get '/surveys/:survey_id' do
   session[:round_id] = Round.create(user_id: session[:id], survey_id: params[:survey_id]).id
   @survey_length = @survey.questions.length
   @questions = @survey.questions
-  @array = [0,1,2,3]
+
   erb :"surveys/survey"
 end
 

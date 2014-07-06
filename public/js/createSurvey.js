e2004d9e9dfbb83dbb912f9b54786d278574d2fc
Zach Pflederer <zpfled@gmail.com>
2014-07-06 14:04:16 -0500

    the wave of the future

--- public/js/createSurvey.js
+++ public/js/createSurvey.js
@@ -6,13 +6,13 @@ $(document).ready(function() {
 
 	$('.create-survey-section').on('click', '#add-question', function(){
 		questionCount++;
-		questionHTML = '<article class="question-container"><input class="form-field user-email" name="questions[question_' + questionCount + '][question]" type="text" placeholder="ask a question" autofocus><span class="btn btn-primary add-choice">add a choice</span><span class="btn btn-secondary rm-question">remove this question</span></article>';
-		$(this).parent().append(questionHTML);
+		questionHTML = '<article class="question-container"><ul class="question-end"><li><input class="form-field" name="questions[question_' + questionCount + '][question]" type="text" placeholder="ask a question" autofocus></li><li><span class="btn btn-primary add-choice">add a choice</span><span class="btn btn-secondary rm-question">remove this question</span></li></ul></article>';
+		$('.survey-title').after(questionHTML);
 	});
 
 	$('.create-survey-section').on('click', '.add-choice', function(){
 		choiceCount++;
-		choiceHTML = '<input class="input_choices" id="choice_' + questionCount + choiceCount + '"><input class="form-field user-email" name="choice_' + questionCount + choiceCount + '" type="text" placeholder = "choice_' + questionCount + choiceCount + '">';
+		choiceHTML = '<input class="input_choices" id="choice_' + questionCount + choiceCount + '"><input class="form-field" name="choice_' + questionCount + choiceCount + '" type="text" placeholder = "choice_' + questionCount + choiceCount + '">';
 		$(this).parent().append(choiceHTML);
 	});
 
@@ -20,11 +20,4 @@ $(document).ready(function() {
 	$('.create-survey-section').on('click', '.rm-question', function(){
 		$(this).parent().remove();
 	});
-
-	// $('.create-survey-section').on('click', '.create-survey-form', function(event){
-	// 	event.preventDefault();
-	// });
-
-
-
 });
